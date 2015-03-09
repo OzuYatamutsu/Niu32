e9e4dab395f85284e6f21fa629e08d7739b531b0
OzuYatamutsu <scollins36@gatech.edu>
2015-03-09 12:47:37 -0400

    Register lookup table

--- n32-assemble.py
+++ n32-assemble.py
@@ -16,6 +16,19 @@ ERR_INVALID_ARGS = """Syntax: n32-assemble.py <filename> [args]
     -o [filename], --output [filename]: Output filename.
     -v, --verbose: Print verbose output."""
 
+### Lookup tables
+REGS = {"zero": "00000", "a0": "00001", "a1": "00010", 
+        "a2": "00011", "a3": "00100", "t0": "00101", 
+        "t1": "00110", "t2": "00111", "t3": "01000", 
+        "t4": "01001", "t5": "01010", "t6": "01011", 
+        "t7": "01100", "s0": "01101", "s1": "01110",
+        "s2": "01111", "s3": "10000", "s4": "10001", 
+        "s5": "10010", "s6": "10011", "s7": "10100", 
+        "r0": "10101", "r1": "10110", "r2": "10111", 
+        "r3": "11000", "ra": "11001", "gp": "11010", 
+        "fp": "11011", "sp": "11100", "at": "11101", 
+        "k0": "11110", "k1": "11111"}
+
 def main():
     # Parse command-line arguments
     if (len(argv) < MIN_ARG_SIZE):
